e1273ce70cba29ddb51e80edb96ad1517cc9f365
rishabhprimafeliticas <rishabh.primafelicitas@gmail.com>
2025-03-26 14:54:59 +0530

    Update web3.js

--- containers/web3.js
+++ containers/web3.js
@@ -14,100 +14,10 @@ import addressbook from "../config/4690.json";
 import { AwareToken__factory } from '../@aware/sdk/typechain';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import Web3 from "web3";
-// Web3Modal provider options
-// const providerOptions = {
-//   walletconnect: {
-//     package: WalletConnectProvider,
-//     options: {
-//       // Inject Infura
-//       infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-//     },
-//   },
-// };
 
-// const provider = new WalletConnectProvider({
-//   rpc: {
-//     4690: process.env.BABEL_ENDPOINT,    
-//     // ...
-//   },
-// });
 
 function useWeb3() {
-  // const [AwareToken, setAwareToken] = useState(null); // Pebble provider
-  // const [modal, setModal] = useState(null); // Web3Modal
-  // const [address, setAddress] = useState(null); // ETH address
-  // const [signer, setSigner] = useState(null); // connected user address
-  // const [AwareTokenBalance, setAwareTokenBalance] = useState(0); // Token balance
-  
-  /**
-   * Setup Web3Modal on page load (requires window)
-   */
-  // const setupWeb3Modal = () => {
-  //   // Creaste new web3Modal
-  //   const web3Modal = new Web3Modal({
-  //     network: "iotextestnet",
-  //     cacheProvider: true,
-  //     provider
-  //   });
-
-  //   // const web3Modal = new Web3Modal({
-  //   //   network: "ropsten",
-  //   //   cacheProvider: true,
-  //   //   providerOptions: providerOptions,
-  //   // });
-
-  //   // Set web3Modal
-  //   setModal(web3Modal);
-  // };
-
-  /**
-   * Authenticate and store necessary items in global state
-   */
-  // const authenticate = async () => {
-  //   // Initiate web3Modal
-  //   const web3Provider = await modal.connect();
-  //   await web3Provider.enable();
-
-  //   // Generate ethers provider
-  //   const provider = new providers.Web3Provider(web3Provider);
-
-  //   console.log(provider);
-  //   // Collect address
-  //   const signer = provider.getSigner();
-  //   const address = await signer.getAddress();
-  //   setAddress(address);
-  //   setSigner(signer);
-
-  //   // Generate Aware provider
-  //   const Awareobj = new awaretoken(signer, 4690, addressbook.awareToken);
-   
-  //   console.log(Awareobj);    
-  //   setAwareToken(Awareobj);
-  // };
-
-  // const getuserAddress = async() => {
-  //   if(signer){
-  //     return await signer.getAddress();
-  //   }
-  //   if(address){
-  //     return address;
-  //   }
-  // }
  
-  // const getMetaDataStr = async( name, date, awareTokenType, awareAssetId, productionFacility, producer, batchNo, valueChainProcess, materialSpecs) => {
-  //   const objJSON = {    
-  //     "name": name,  
-  //     "date": date,
-  //     "awareTokenType": awareTokenType,
-  //     "awareAssetId": awareAssetId,
-  //     "productionFacility": productionFacility,
-  //     "producer": producer,
-  //     "batchNo": batchNo,
-  //     "valueChainProcess": valueChainProcess,
-  //     "materialSpecs": materialSpecs
-  //   }
-  //   return JSON.stringify(objJSON);
-  // } 
   /**
    * Converts File to an ArrayBuffer for hashing preperation
    * @param {File} file uploaded file
@@ -136,7 +46,6 @@ function useWeb3() {
    */
   const mintAwareToken = async (file, name, awaremetadata, useraddress, amount ) => {
     
-    // console.log(file, name, awaremetadata.description, awaremetadata.sustainableProcessClaim);
     // Generate metadataJSON
     const metadataJSON = generateMetadata("aware-20221012", {
       description: awaremetadata.description ? awaremetadata.description : "",
@@ -185,27 +94,12 @@ function useWeb3() {
       metadataHash
     );
 
-    // console.log("awareData", JSON.stringify(awareData));
-    // console.log("useraddress", useraddress, amount);
-        
-    // const tx = await AwareToken.mint(awareData, useraddress, amount );
-    // await tx.wait(1); // Wait 1 confirmation and throw user to next screen
+    
     
   };
 
 
-  // On load events
-  // useEffect(setupWeb3Modal, []);
-
-  // return {
-  //   address,
-  //   mintAwareToken,    
-  //   getMetaDataStr,
-  //   authenticate,    
-  //   getuserAddress,    
-  // };
+ 
 }
 
-// // Create unstate-next container
-// const web3 = createContainer(useWeb3);
-// export default web3;
+
