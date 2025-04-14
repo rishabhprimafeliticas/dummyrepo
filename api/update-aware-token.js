ac7de08ee74eb871700767e1c936f7f8b7257e0e
rishabhprimafeliticas <rishabh.primafelicitas@gmail.com>
2025-04-14 21:47:55 +0530

    Update update-aware-token.js

--- api/update-aware-token.js
+++ api/update-aware-token.js
@@ -1,100 +1,124 @@
 const express = require("express");
-var asyncHandler = require('express-async-handler');
-const { body, validationResult, check } = require('express-validator');
-router = express.Router();;
-var updateawaretokenhandler = require('../services/update-aware-token-handler');
-const { verify } = require('../middleware');
-var _ = require('lodash');
-var multer = require('multer');
+var asyncHandler = require("express-async-handler");
+const { body, validationResult, check } = require("express-validator");
+router = express.Router();
+var updateawaretokenhandler = require("../services/update-aware-token-handler");
+const { verify } = require("../middleware");
+var _ = require("lodash");
+var multer = require("multer");
+const {
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  ensureUpdateTokenAccessibleAndHandleDeleteLock,
+} = require("../middleware/tokenLockMiddleware");
 var limits = {
-    // allow only 1 file per request
-    fileSize: 7340032, // 2 MB (max file size)
+  // allow only 1 file per request
+  fileSize: 7340032, // 2 MB (max file size)
 };
 var fileFilters = function (req, file, cb) {
-    // supported image file mimetypes
-    var allowedMimes = ['application/pdf', 'image/png', 'image/heif', 'image/jpg', 'image/jpeg', 'image/pjpeg', 'image/heic'];
+  // supported image file mimetypes
+  var allowedMimes = [
+    "application/pdf",
+    "image/png",
+    "image/heif",
+    "image/jpg",
+    "image/jpeg",
+    "image/pjpeg",
+    "image/heic",
+  ];
 
-    if (_.includes(allowedMimes, file.mimetype)) {
-        // allow supported image files
-        cb(null, true);
-    } else {
-        // throw error for invalid files
-        cb(new Error('I know you have it in you, Try again!'));
-        // return res.status(400).jsonp('I know you have it in you, Try again!')
-
-    }
+  if (_.includes(allowedMimes, file.mimetype)) {
+    // allow supported image files
+    cb(null, true);
+  } else {
+    // throw error for invalid files
+    cb(new Error("I know you have it in you, Try again!"));
+    // return res.status(400).jsonp('I know you have it in you, Try again!')
+  }
 };
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function (req, file, cb) {
-        // const originalname = file.originalname;
-        // const extension = originalname.substring(originalname.lastIndexOf('.')); // Get file extension
-        // const filename = originalname.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]/g, ''); // Replace spaces and non-English characters
-        // cb(null, Date.now() + filename + extension);
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    // const originalname = file.originalname;
+    // const extension = originalname.substring(originalname.lastIndexOf('.')); // Get file extension
+    // const filename = originalname.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]/g, ''); // Replace spaces and non-English characters
+    // cb(null, Date.now() + filename + extension);
+    cb(null, file.originalname);
+  },
 });
 var upload = multer({
-
-    fileFilter: fileFilters,
-    storage: storage,
-
+  fileFilter: fileFilters,
+  storage: storage,
 });
-
 
 var storage2 = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function (req, file, cb) {
-
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 var upload2 = multer({
-
-    fileFilter: fileFilters,
-    storage: storage2,
-
+  fileFilter: fileFilters,
+  storage: storage2,
 });
 
+router.post(
+  "/v2/update_aware_token",
+  [],
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.updateAwareTokenAsync)
+);
 
-router.post('/v2/update_aware_token',
-    [
+router.post(
+  "/v2/create_draft_info_async",
+  [],
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.createDraftInfoAsync)
+);
+router.post(
+  "/v2/update_purchase_order_async",
+  [],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.updatePurchaseOrderAsync)
+);
 
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.updateAwareTokenAsync));
+router.get(
+  "/v2/get_draft_info_list_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getDraftInfoListAsync)
+);
 
+router.get(
+  "/v2/get_draft_info_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getDraftInfoAsync)
+);
 
-router.post('/v2/create_draft_info_async',
-    [
+router.get(
+  "/v2/get_updated_aware_token_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedAwareTokensAsync)
+);
 
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.createDraftInfoAsync));
-router.post('/v2/update_purchase_order_async',
-    [],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.updatePurchaseOrderAsync));
+router.get(
+  "/v2/get_all_update_aware_token_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getAllUpdateAwareTokenAsync)
+);
 
-router.get('/v2/get_draft_info_list_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getDraftInfoListAsync));
-
-
-router.get('/v2/get_draft_info_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getDraftInfoAsync));
-
-router.get('/v2/get_updated_aware_token_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdatedAwareTokensAsync));
-
-router.get('/v2/get_all_update_aware_token_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getAllUpdateAwareTokenAsync));
-
-router.get('/v2/get_all_update_aware_token_manager_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getallupdateAwareTokenManagerAsync));
+router.get(
+  "/v2/get_all_update_aware_token_manager_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.getallupdateAwareTokenManagerAsync
+  )
+);
 
 // router.get('/v2/get_aware_token_transaction_history_async', verify,
 //     asyncHandler(awaretokenhandler.handlers.getAwareTokenTransactionHistoryAsync));
@@ -130,95 +154,139 @@ router.get('/v2/get_all_update_aware_token_manager_async', verify,
 //     asyncHandler(awaretokenhandler.handlers.getSourceAddressAsync));
 
 // //done
-router.post('/v2/select_update_aware_token_type_async',
-    [
-        // check('_awareid_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('date_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('production_facility_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-
-        // check('aware_token_type_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('main_color_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('select_main_color_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('production_lot_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('weight_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('sustainable_process_claim_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('wet_processing_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-
-
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.selectUpdateAwareTokenTypeAsync));
-
-//done
-router.get('/v2/get_selected_update_aware_token_type_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getSelectedUpdateAwareTokenTypeAsync));
-
+router.post(
+  "/v2/select_update_aware_token_type_async",
+  [
+    // check('_awareid_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('date_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('production_facility_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('aware_token_type_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('main_color_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('select_main_color_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('production_lot_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('weight_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('sustainable_process_claim_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('wet_processing_t').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+  ],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.selectUpdateAwareTokenTypeAsync)
+);
 
 //done
-router.post('/v2/update_physical_assest_async',
-    [
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.updatePhysicalAssestAsync));
+router.get(
+  "/v2/get_selected_update_aware_token_type_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.getSelectedUpdateAwareTokenTypeAsync
+  )
+);
 
 //done
-router.get('/v2/get_updated_physical_assets', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdatedPhyscialAssetsAsync));
-
-
-//done
-router.post('/v2/update_tracer', upload.single("update_pdf"), [], verify,
-    asyncHandler(updateawaretokenhandler.handlers.updateTracerAsync));
-
-
-//done
-router.get('/v2/get_updated_tracer', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdatedTracerAsync));
-
+router.post(
+  "/v2/update_physical_assest_async",
+  [],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.updatePhysicalAssestAsync)
+);
 
 //done
-router.post('/v2/update_post_self_validation_async', upload2.array('uploadedImages[]'),
-    [
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.updateSelfValidationAsync));
+router.get(
+  "/v2/get_updated_physical_assets",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedPhyscialAssetsAsync)
+);
 
 //done
-router.get('/v2/get_updated_self_validation', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdatedSelfValidationAsync));
-
-
-//done
-router.post('/v2/update_company_complinces_async',
-    [
-        check('_awareid').escape(),
-        // check('environmental_scope_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('social_compliance_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-        // check('chemical_compliance_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.updateCompanyComplianceAsync));
+router.post(
+  "/v2/update_tracer",
+  upload.single("update_pdf"),
+  [],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.updateTracerAsync)
+);
 
 //done
-router.get('/v2/get_updated_company_compliance', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdateCompanyComplianceAsync));
+router.get(
+  "/v2/get_updated_tracer",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedTracerAsync)
+);
 
 //done
-router.post('/v2/post_updated_digital_twin',
-    [
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.postUpdatedDigitalTwinAsync));
+router.post(
+  "/v2/update_post_self_validation_async",
+  upload2.array("uploadedImages[]"),
+  [],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.updateSelfValidationAsync)
+);
 
 //done
-router.get('/v2/get_updated_digital_twin', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdatedDigitalTwinAsync));
+router.get(
+  "/v2/get_updated_self_validation",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedSelfValidationAsync)
+);
 
+//done
+router.post(
+  "/v2/update_company_complinces_async",
+  [
+    check("_awareid").escape(),
+    // check('environmental_scope_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('social_compliance_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+    // check('chemical_compliance_certificates').not().isEmpty().withMessage("I know you have it in you, Try again!").escape(),
+  ],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.updateCompanyComplianceAsync)
+);
 
+//done
+router.get(
+  "/v2/get_updated_company_compliance",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdateCompanyComplianceAsync)
+);
 
-router.get('/v2/get_my_final_brand_connections', [],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.getMyFinalBrandConnectionsAsync));
+//done
+router.post(
+  "/v2/post_updated_digital_twin",
+  [],
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenEditableAndLock,
+  asyncHandler(updateawaretokenhandler.handlers.postUpdatedDigitalTwinAsync)
+);
+
+//done
+router.get(
+  "/v2/get_updated_digital_twin",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedDigitalTwinAsync)
+);
+
+router.get(
+  "/v3/get_updated_digital_twin",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdatedDigitalTwinAsyncV3)
+);
+
+router.get(
+  "/v2/get_my_final_brand_connections",
+  [],
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getMyFinalBrandConnectionsAsync)
+);
 // //done
 // router.post('/v2/post_self_validation_async', upload.array('uploadedImages[]'),
 //     [
@@ -232,13 +300,14 @@ router.get('/v2/get_my_final_brand_connections', [],
 //     asyncHandler(awaretokenhandler.handlers.getSelfValidationAsync));
 
 //done
-router.post('/v2/reset_update_aware_token',
-    [
-        check('_awareid').escape(),
-        check('type').escape(),
-    ],
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.deleteResetUpdateAwareTokenAsync));
+router.post(
+  "/v2/reset_update_aware_token",
+  [check("_awareid").escape(), check("type").escape()],
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.deleteResetUpdateAwareTokenAsync
+  )
+);
 
 // //done
 // router.post('/v2/post_tracer',
@@ -263,56 +332,95 @@ router.post('/v2/reset_update_aware_token',
 // router.get('/v2/get_digital_twin', verify,
 //     asyncHandler(awaretokenhandler.handlers.getDigitalTwinAsync));
 
-router.post('/v2/delete_update_aware_token',
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.deleteUpdateAwareTokenAsync));
+router.post(
+  "/v2/delete_update_aware_token",
+  verify,
+  ensureUpdateTokenExists,
+  ensureUpdateTokenAccessibleAndHandleDeleteLock,
+  asyncHandler(updateawaretokenhandler.handlers.deleteUpdateAwareTokenAsync)
+);
 
 //done
-router.get('/v2/get_update_aw_token_details', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdateAwareTokenDetailsAsync));
+router.get(
+  "/v2/get_update_aw_token_details",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdateAwareTokenDetailsAsync)
+);
 
 // router.get('/v2/get_wallet_async', verify,
 //     asyncHandler(awaretokenhandler.handlers.getWalletAsync));
 
-
 ///we have to do it for both create token and updated token
-router.get('/v2/get_received_aware_tokens_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getReceivedAwareTokensAsync));
+router.get(
+  "/v2/get_received_aware_tokens_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getReceivedAwareTokensAsync)
+);
 
+router.get(
+  "/v2/get_historical_tracers_of_aware_asset",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.getHistoricalTracersOfAwareAssetsAsync
+  )
+);
 
-router.get('/v2/get_historical_tracers_of_aware_asset', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getHistoricalTracersOfAwareAssetsAsync));
+router.get(
+  "/v2/get_update_digital_twin",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getUpdateDigitalTwinAsync)
+);
 
-router.get('/v2/get_update_digital_twin', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getUpdateDigitalTwinAsync));
-
-router.get('/v2/get_details_for_update_self_validation_certificate_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getDetailsForUpdateSelfValidationCertificateAsync));
-
+router.get(
+  "/v2/get_details_for_update_self_validation_certificate_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers
+      .getDetailsForUpdateSelfValidationCertificateAsync
+  )
+);
 
 //03/12/2022
-router.get('/v2/get_purchase_orders_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getparticularPurchaseOrdersFromBrandAsync));
+router.get(
+  "/v2/get_purchase_orders_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.getparticularPurchaseOrdersFromBrandAsync
+  )
+);
 
-router.get('/v2/get_product_lines_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getProductLineFromBrandAsync));
+router.get(
+  "/v2/get_product_lines_async",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.getProductLineFromBrandAsync)
+);
 
-router.get('/v2/get_particular_product_line_async', verify,
-    asyncHandler(updateawaretokenhandler.handlers.getParticularProductLineFromBrandAsync));
+router.get(
+  "/v2/get_particular_product_line_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.getParticularProductLineFromBrandAsync
+  )
+);
 
-router.post('/v2/send_pos_to_admin_for_approval',
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.sendPosToAdminForApprovalAsync));
+router.post(
+  "/v2/send_pos_to_admin_for_approval",
+  verify,
+  asyncHandler(updateawaretokenhandler.handlers.sendPosToAdminForApprovalAsync)
+);
 
-router.post('/v2/delete_update_aware_token_po_line_async',
-    verify,
-    asyncHandler(updateawaretokenhandler.handlers.deleteUpdateAwareTokenPoLineAsync));
+router.post(
+  "/v2/delete_update_aware_token_po_line_async",
+  verify,
+  asyncHandler(
+    updateawaretokenhandler.handlers.deleteUpdateAwareTokenPoLineAsync
+  )
+);
 
+router.get(
+  "/v2/for_CID",
 
-    router.get('/v2/for_CID',
-     
-        asyncHandler(updateawaretokenhandler.handlers.for_CID));
+  asyncHandler(updateawaretokenhandler.handlers.for_CID)
+);
 
 module.exports = router;
-
-
